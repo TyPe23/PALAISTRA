1841618ac7d6feb64292411d3d9763aaf69166e8
reberaidan <aidanweinreber@hotmail.com>
2023-04-27 17:47:34 -0500

    vase to item interaction
    Added vases. Can put different prefabs into vase and they will pop out.

---
+++ Assets/Scripts/Interactables/CoinSpawn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawn : MonoBehaviour
+{
+    [SerializeField] private int worth;
+    private Collider collision;
+    private void Awake()
+    {
+        collision = GetComponent<Collider>();
+        collision.enabled = false;
+    }
+    void Start()
+    {
+
+        StartCoroutine(waitAfterSpawn());
+        
+    }
+
+    IEnumerator waitAfterSpawn()
+    {
+        yield return new WaitForSeconds(3);
+        collision.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            print("You earned " + worth);
+            //TODO add money to character inventory
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Interactables/VasePhysics.cs
+++ Assets/Scripts/Interactables/VasePhysics.cs
@@ -5,23 +5,14 @@ using UnityEngine;
 
 public class VasePhysics : MonoBehaviour
 {
-
+    [Tooltip("Input the possible collidable items tags that can break")]
     [SerializeField] private List<string> collidables;
-    [SerializeField] private GameObject player;
-    private BoxCollider boxtrigger;
-    private Rigidbody body;
+    [SerializeField] private List<GameObject> spawns;
     private bool sprint;
+
 
     // Start is called before the first frame update
     void Start()
-    {
-
-        boxtrigger = GetComponent<BoxCollider>();
-        body = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
     {
     }
 
@@ -35,10 +26,12 @@ public class VasePhysics : MonoBehaviour
         {
             if (other.transform.CompareTag(possible)&&sprint)
             {
-
-                print("Entering Trigger");
-                body.useGravity = true;
-                
+                int chance = Random.Range(1, 5);
+                print(chance);
+                if(chance == 4) {
+                    Instantiate(spawns[0], transform.position, Quaternion.identity);
+                }
+                Destroy(gameObject);
             }
         }
     }
