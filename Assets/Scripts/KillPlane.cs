2f40847234225d566572cb81ec04fb97b40fa2d5
reberaidan <aidanweinreber@hotmail.com>
2023-05-03 00:54:26 -0500

    start of alpha test

---
+++ Assets/Scripts/KillPlane.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlane : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private Vector3 origin;
+
+    private void Start()
+    {
+        origin = player.transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            player.GetComponent<CharacterController>().enabled = false;
+            player.transform.position = origin;
+            player.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+}
