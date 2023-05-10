7718c79290f1eac7d468ae50b3bb56a0dc4016f9
reberaidan <aidanweinreber@hotmail.com>
2023-05-10 02:26:58 -0500

    room 1 design

--- Assets/Scripts/Interactables/Gate.cs
+++ Assets/Scripts/Interactables/Gate.cs
@@ -14,27 +14,29 @@ public enum direction
 public class Gate : MonoBehaviour
 {
     private GameObject roomMan;
+    private GameObject player;
 
     [Tooltip("Put at -1 to randomly choose room, otherwise, input buildIndex of next room")]
     [SerializeField] int nextRoom;
 
-    [Tooltip("Determines the hightest point on the gate.")]
-    [SerializeField] private int peak;
-
+    [SerializeField] private Collider topCollider;
+    [SerializeField] private Collider bottomCollider;
     [SerializeField] private direction goDir;
+    [SerializeField] private MeshRenderer text;
     private bool leaving;
     
     // Start is called before the first frame update
     void Start()
     {
         roomMan = GameObject.Find("RoomManager");
+        player = GameObject.FindWithTag("Player");
+        text.enabled = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
         if (leaving) {
-            var player = GameObject.FindWithTag("Player");
             if (goDir == direction.left)
             {
                 player.GetComponent<StarterAssetsInputs>().move = new Vector2(-0.7f, 0.7f);
@@ -58,18 +60,28 @@ public class Gate : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        text.enabled = true;
+    }
     private void OnTriggerStay(Collider other)
     {
         
-        if (other.transform.CompareTag("Player")&& roomMan.GetComponent<RoomManager>().levelComplete)
+        if (other.transform.CompareTag("Player")&& roomMan.GetComponent<RoomManager>().levelComplete && player.GetComponent<StarterAssetsInputs>().interact)
         {
-            
+            bottomCollider.enabled = false;
             StartCoroutine(BeforeSceneChange());
+            topCollider.enabled = false;
             
         }
     }
 
-    
+    private void OnTriggerExit(Collider other)
+    {
+        text.enabled = false;
+    }
+
+
     private IEnumerator BeforeSceneChange()
     {
 
