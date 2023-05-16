f506c09b534685a3af1613df34f69b586aa91c23
Ty Pederson <ty23117@gmail.com>
2023-05-15 21:41:02 -0500

    end of game fix

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -8,7 +8,10 @@ public class MusicManager : MonoBehaviour
     public AudioSource audioCombat;
     public AudioSource audioLoop;
 
+    public Enemy[] enemies;
+
     private bool startedLoop = false;
+    private bool inCombat = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +22,39 @@ public class MusicManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!audioCombat.isPlaying && !startedLoop)
+        if (enemies[0].state == enemyStates.MOVE && !inCombat)
+        {
+            inCombat = true;
+        }
+
+        if (!audioBase.isPlaying && inCombat && !startedLoop)
+        {
+            audioCombat.Play();
+        }
+        else if (!inCombat && !audioBase.isPlaying)
+        {
+            audioBase.Play();
+        }
+
+        if (!audioCombat.isPlaying && !startedLoop && inCombat)
         {
             startedLoop = true;
             audioLoop.Play();
+        }
+
+        if (inCombat)
+        {
+            foreach  (Enemy enemy in enemies)
+            {
+                if (enemy.health <= 0)
+                {
+                    inCombat = false;
+                }
+                else
+                {
+                    inCombat = true;
+                }
+            }
         }
     }
 }
